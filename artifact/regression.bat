b0f9132ea778e8956a866cf5f1530621e16ee00e
Balakrishna Reddy Singamreddy <Balakrishna.Singamreddy@ATMECS.CORP>
2019-11-25 19:14:08 +0530

    added batch files

---
+++ artifact/regression.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+cd C:\projects\nexial-core\bin\
+nexial.cmd -plan C:\projects\MyFirstProject\artifact\plan\MyFirstProject-plan.xlsx,C:\projects\MyFirstProject\artifact\plan\Test-plan.xlsx,
---
+++ artifact/req_batch.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+echo %1
+cd C:\projects\nexial-core\bin\
+nexial.cmd -plan C:\projects\MyFirstProject\artifact\plan\%1
+exit																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																									
