bcb0c48a0473409c417efafe568e4b27898612f9
Balakrishna Reddy Singamreddy <Balakrishna.Singamreddy@ATMECS.CORP>
2019-11-26 11:31:34 +0530

    updated bat file

--- artifact/req_batch.bat
+++ artifact/req_batch.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 echo %1
+echo %2
 cd C:\Users\balakrishna.s\.jenkins\workspace\Nexial_Project\nexial-core\bin\
-nexial.cmd -plan C:\Users\balakrishna.s\.jenkins\workspace\Nexial_Project\artifact\plan\Test-plan.xlsx
+nexial.cmd -plan %1\artifact\plan\%2
 exit																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																									
